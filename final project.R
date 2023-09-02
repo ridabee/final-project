d8267aea32ce4c2ade77b88b29a57a19aca0184a
ridabee <rida.akbar@emory.edu>
2023-09-02 02:52:45 -0400

    complete

--- final project.R
+++ final project.R
@@ -1,18 +1,49 @@
 # EPI 550 Final Project
+install.packages("usethis")
+usethis::use_git_config(user.name = "ridabee" , use.email="rida.akbar@emory.edu")
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+												install.packages("renv")
+												renv::init()
+												install.packages("here")
+load(file = 'covid_testing.rda')
 
-install.packages("dplyr")
-library(dplyr)
-
-load("/Users/ridabee/Desktop/EPI 590R Final Project/covid_testing.rda")
-
+#Decriptive stats
+View(covid_testing)
 library(gtsummary)
+tbl_summary(
+	covid_testing,
+	by = gender,
+	include = c( age, result, clinic_name, test_id, pan_day, demo_group, patient_class))
 
-tbl <- tbl_summary(covid_testing)
+#Regression
+logistic_model <- glm(orderset ~ gender +test_id, data=covid_testing, family = binomial())
+tbl_regression(
+	logistic_model,
+	intercept = TRUE,
+	label = list(
+		test_id ~ "Subject ID for testing",
+		orderset ~ "Sequence",
+		gender ~ "sex assigned at birth"
+	))
+#usingbroom
+library(broom)
+glance(logistic_model, conf.int = TRUE, exponentiate = TRUE)
 
-covid_testing_df <- as.data.frame(covid_testing)
+#Figure
+box <- boxplot(covid_testing$age ~ covid_testing$gender,
+							 xlab = "gender
+							 ylab = age",
+							 main= "Subjects age by gender")
+install.packages("ggplot2")
+library(ggplot2)
+ggsave(here::here("Plots", "box.jpg"))
 
-write.table(covid_testing_df, file = "covid_testing_data.txt", sep = "\t", row.names = FALSE)
+#Function
+#Percentage of demographic group from gender
 
-write.table(covid_testing_df, file = "/Users/ridabee/Desktop/EPI 590R Final Project/covid_testing_data.txt", sep = "\t", row.names = FALSE)
-
-
+result <- function (x,y, multiplier = 100){
+	new_value = multiplier*sum(x)/sum(y)
+	return(new_value)
+}
+result(covid_testing$result = "result", covid_testing$gender="sex")
